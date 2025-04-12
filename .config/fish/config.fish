5e59615229f0b0a34c60e61e991088fffbbfffb1
ThetaOmega01 <zjhztjf@gmail.com>
2025-04-12 21:34:15 +0800

    Add fish and nvim

---
+++ .config/fish/config.fish
@@ -0,0 +1,5 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    starship init fish | source
+    alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+end
