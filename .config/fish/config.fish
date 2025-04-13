a037d1657479d73a1a387f81f396ad84c0c2626f
stea <stea@steadeMac-mini.local>
2025-04-13 14:17:31 +0800

    remove fish

--- .config/fish/config.fish
+++
@@ -1,5 +0,0 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-    starship init fish | source
-    alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
-end
