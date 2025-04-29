727b66fa050fb6c29409cf55c0e0a42cba4ec805
ThetaOmega01 <zjhztjf@gmail.com>
2025-04-29 12:14:03 +0800

    Use JB theme and remove ascii art

---
+++ .config/nvim/lua/plugins/extend-colorscheme.lua
@@ -0,0 +1,11 @@
+return {
+  -- add kanagawa
+  { "nickkadutskyi/jb.nvim" },
+  {
+    "Lazyvim/LazyVim",
+    opts = {
+      colorscheme = "jb",
+      style = "dragon",
+    },
+  },
+}
---
+++ .config/nvim/lua/plugins/extend-snacks.lua
@@ -0,0 +1,11 @@
+return {
+  "snacks.nvim",
+  opts = {
+    dashboard = {
+      preset = {
+        -- Set the header to an empty string to remove the ASCII art
+        header = "",
+      },
+    },
+  },
+}
