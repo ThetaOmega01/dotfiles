568cebd82b5cabb900c5758d15390d314ebd8e87
ThetaOmega01 <zjhztjf@gmail.com>
2025-06-10 10:20:43 +0800

    - remove unused

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,20 +8,6 @@ if status is-interactive
     set fish_greeting ''
     fish_vi_key_bindings
     add_abbr
-
-    # ASDF configuration code
-    if test -z $ASDF_DATA_DIR
-        set _asdf_shims "$HOME/.asdf/shims"
-    else
-        set _asdf_shims "$ASDF_DATA_DIR/shims"
-    end
-
-    # Do not use fish_add_path (added in Fish 3.2) because it
-    # potentially changes the order of items in PATH
-    if not contains $_asdf_shims $PATH
-        set -gx --prepend PATH $_asdf_shims
-    end
-    set --erase _asdf_shims
 end
 
 # Added by OrbStack: command-line tools and integration
