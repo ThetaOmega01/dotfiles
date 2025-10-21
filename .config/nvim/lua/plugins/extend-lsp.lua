b5beeb8ad3a82300fbd5d2fc4d0fd49b2329450e
ThetaOmega01 <zjhztjf@gmail.com>
2025-10-21 12:00:07 +0900

    - configure pyright lsp

--- .config/nvim/lua/plugins/extend-lsp.lua
+++ .config/nvim/lua/plugins/extend-lsp.lua
@@ -1,40 +1,27 @@
 return {
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      -- Server-specific configurations
-      servers = {
-        -- Configure Pyright
-        pyright = {
+  "neovim/nvim-lspconfig",
+  opts = {
+    servers = {
+      jedi_language_server = {},
+      ruff = {
+        init_options = {
           settings = {
-            pyright = {
-              disableTaggedHints = true,
-              disableOrganizeImports = true, -- Let Ruff handle this via <leader>co
-            },
-            python = {
-              analysis = {
-                typeCheckingMode = "basic", -- or "strict"
-                diagnosticSeverityOverrides = {
-                  -- Rules Ruff handles well are set to "none" in Pyright
-                  reportUnusedImport = "none", -- Ruff: F401
-                  reportUnusedVariable = "none", -- Ruff: F841
-                  reportUnusedFunction = "none", -- Ruff handles this
-                  reportUndefinedVariable = "none", -- Ruff: F821
-                  reportGeneralTypeIssues = "warning", -- Keep Pyright's type checking diagnostics
-                },
-              },
+            lint = {
+              enable = false,
             },
           },
         },
       },
-
-      -- Setup function to fine-tune capabilities after attach
-      setup = {
-        ruff_lsp = function(client)
-          -- Disable Ruff's hover provider to prefer Pyright's richer type info
-          client.server_capabilities.hoverProvider = false
-        end,
-      },
+    },
+    setup = {
+      pyright = function()
+        require("lazyvim.util").lsp.on_attach(function(client, _)
+          if client.name == "pyright" then
+            -- disable hover in favor of jedi-language-server
+            client.server_capabilities.hoverProvider = false
+          end
+        end)
+      end,
     },
   },
 }
