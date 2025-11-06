f9be9744249453a9a7fcab2c82fb7d61a9654272
ThetaOmega01 <zjhztjf@gmail.com>
2025-11-06 09:34:19 +0900

    - VimTeX: Switch to Tectonic - Starship: timeout to 1000ms

--- .config/nvim/lua/plugins/extend-vimtex.lua
+++ .config/nvim/lua/plugins/extend-vimtex.lua
@@ -1,6 +1,10 @@
 return {
   "lervag/vimtex",
   config = function()
+    vim.g.vimtex_compiler_method = "tectonic"
     vim.g.vimtex_syntax_conceal_disable = 1
+    vim.g.vimtex_compiler_tectonic = {
+      options = { "--keep-logs", "--synctex" },
+    }
   end,
 }
