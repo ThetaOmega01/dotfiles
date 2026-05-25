03ff7b99769ec0a7fff089925191727a18ed8e9a
ThetaOmega01 <zjhztjf@gmail.com>
2026-05-25 13:27:06 +0900

    chore: change to modus_vivendi theme

--- .config/nvim/lua/plugins/extend-colorscheme.lua
+++ .config/nvim/lua/plugins/extend-colorscheme.lua
@@ -1,6 +1,9 @@
 return {
-  "LazyVim/LazyVim",
-  opts = {
-    colorscheme = "catppuccin",
+  { "miikanissi/modus-themes.nvim" },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "modus_vivendi",
+    },
   },
 }
